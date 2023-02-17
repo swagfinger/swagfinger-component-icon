b0101931e55f0c47244bb15ae544fb2759809b31
swagfinger <swagfinger@gmail.com>
2023-02-17 23:50:35 +0800

    remove webpack

--- src/index.js
+++ src/index.js
@@ -1,17 +1,1 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Icon } from './component/Icon';
-import { ShoppingCartIcon } from '@heroicons/react/24/outline';
-
-if (process.env.NODE_ENV === 'development') {
-  const container = document.getElementById('root');
-  const root = createRoot(container);
-
-  root.render(
-    <Icon iconSize='30px' color='error'>
-      <ShoppingCartIcon />
-    </Icon>
-  );
-}
-
-export * from './component/Icon';
+export * from './Icon';
