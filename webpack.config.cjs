316c809996c3f55ce49a29d7727b7993c1363f93
swagfinger <swagfinger@gmail.com>
2023-02-18 13:22:23 +0800

    patch

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,76 +1,67 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  return {
-    mode: isProduction ? 'production' : 'development',
-    devServer: {
-      static: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 9000,
-    },
-    target: 'web',
-    entry: isProduction
-      ? './src/index.production.js'
-      : './src/index.development.js',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'index.js',
-      library: 'Icon',
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-    },
+module.exports = {
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'Icon',
+  },
 
-    resolve: {
-      extensions: ['.js', 'jsx', '.mjs'], // tell webpack to resolve .mjs files as ES modules
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
+  resolve: {
+    extensions: ['.js', 'jsx', '.mjs'], // tell webpack to resolve .mjs files as ES modules
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
 
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'], // specify react preset
-            },
-          },
-          resolve: {
-            fullySpecified: false,
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], // specify react preset
           },
         },
-        {
-          test: /\.css$/, // process .css files
-          use: [
-            'style-loader', // generate the necessary code to apply styles to components
-            'css-loader', // parse the CSS code
-            {
-              loader: 'postcss-loader',
-              options: {
-                postcssOptions: {
-                  plugins: [
-                    [
-                      'postcss-preset-env',
-                      {
-                        // Options
-                      },
-                    ],
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.css$/, // process .css files
+        use: [
+          'style-loader', // generate the necessary code to apply styles to components
+          'css-loader', // parse the CSS code
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  [
+                    'postcss-preset-env',
+                    {
+                      // Options
+                    },
                   ],
-                },
+                ],
               },
             },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        filename: './index.html',
-        inject: true,
-      }),
+          },
+        ],
+      },
     ],
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: './index.html',
+      inject: true,
+    }),
+  ],
 };
