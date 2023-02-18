ec294782151e45e9cecab5d5b1d8efb19d329ec0
swagfinger <swagfinger@gmail.com>
2023-02-18 13:02:12 +0800

    patch

---
+++ src/index.development.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Icon } from './Icon';
+import { ShoppingCartIcon } from '@heroicons/react/24/outline';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <Icon iconSize='30px' color='error'>
+    <ShoppingCartIcon />
+  </Icon>
+);
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,66 +1,76 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  target: 'web',
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-  },
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
-  },
-  resolve: {
-    extensions: ['.js', 'jsx', '.mjs'], // tell webpack to resolve .mjs files as ES modules
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000,
+    },
+    target: 'web',
+    entry: isProduction
+      ? './src/index.production.js'
+      : './src/index.development.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      library: 'Icon',
+      libraryTarget: 'umd',
+      umdNamedDefine: true,
+    },
 
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'], // specify react preset
-          },
-        },
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.css$/, // process .css files
-        use: [
-          'style-loader', // generate the necessary code to apply styles to components
-          'css-loader', // parse the CSS code
-          {
-            loader: 'postcss-loader',
+    resolve: {
+      extensions: ['.js', 'jsx', '.mjs'], // tell webpack to resolve .mjs files as ES modules
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+
             options: {
-              postcssOptions: {
-                plugins: [
-                  [
-                    'postcss-preset-env',
-                    {
-                      // Options
-                    },
-                  ],
-                ],
-              },
+              presets: ['@babel/preset-env', '@babel/preset-react'], // specify react preset
             },
           },
-        ],
-      },
+          resolve: {
+            fullySpecified: false,
+          },
+        },
+        {
+          test: /\.css$/, // process .css files
+          use: [
+            'style-loader', // generate the necessary code to apply styles to components
+            'css-loader', // parse the CSS code
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [
+                    [
+                      'postcss-preset-env',
+                      {
+                        // Options
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: './index.html',
+        inject: true,
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './index.html',
-      filename: './index.html',
-      inject: true,
-    }),
-  ],
+  };
 };
