f4908fe40e33cb29ff64fffb9071ec24f0f9868c
swagfinger <swagfinger@gmail.com>
2023-02-19 10:44:32 +0800

    remove circular dependency

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -2,17 +2,19 @@ import React from 'react';
 import '../../main.css';
 import styled from 'styled-components';
 
-const IconContainer = styled.div`
+// Extract the functional component
+const IconContainerInner = (props) => <div {...props} />;
+
+// Define the styled component using the extracted component
+const IconContainer = styled(IconContainerInner)`
   width: ${(props) => props.iconSize};
   height: ${(props) => props.iconSize};
   display: flex;
 
-  //color affects the svg if svg has currentColor set somewhere
   color: ${(props) => props.color};
-  // whatever is passed through as children
   > * {
-    width: 100%;
-    height: 100%;
+    width: ${(props) => props.iconSize};
+    height: ${(props) => props.iconSize};
   }
 `;
 
