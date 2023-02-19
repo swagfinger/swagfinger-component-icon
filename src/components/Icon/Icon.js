6d70b87bec4f22a776088e972e135da34fcaa92f
swagfinger <swagfinger@gmail.com>
2023-02-19 14:04:20 +0800

    added emotion to replace styled components

--- src/components/Icon/Icon.js
+++ src/components/Icon/Icon.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 
 const IconContainerInner = ({ iconSize, color, ...rest }) => (
   <div {...rest} style={{ width: iconSize, height: iconSize, color }} />
