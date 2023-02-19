4ac8fb1457825cadfe6fbdd69cbb5c8eda598e9e
swagfinger <swagfinger@gmail.com>
2023-02-19 23:16:45 +0800

    patch - remove styled-components

--- src/Icon/index.js
+++ src/Icon/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const IconContainer = styled(({ children, color }) => (
-  <div style={{ color: color }}>{children}</div>
-))``;
 
 const Icon = ({ children, color }) => {
-  return <IconContainer color={color}>{children}</IconContainer>;
+  return <div>{children}</div>;
 };
 
 export default Icon;
